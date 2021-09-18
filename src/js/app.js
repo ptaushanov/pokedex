f1b572e9ae8afec1977a2474fa049e9a05bf5776
ptaushanov <pepi15032000@gmail.com>
2021-09-19 01:17:38 +0300

    refactor: made fetch code more tidy

--- src/js/app.js
+++ src/js/app.js
@@ -6,28 +6,30 @@ window.addEventListener("DOMContentLoaded", () => {
   const ul = document.querySelector("ul");
   const URL = "https://pokeapi.co/api/v2/pokemon?limit=10"
 
-  fetch(URL, {
-    method: "GET",
-    "Content-Type": "application/json"
-  }).then(res => {
-    if(res.status === 200)
-    {
+  const handleResponce = (res) => {
+    if(res.status === 200){
       return res.json();
     }
     else {
       throw new Error(`Can't get resources. Status: ${res.status}`);
     }
-  }).then(data => {
+  }
+
+  const handleData = (data) => {
     const pokemons = data.results
     pokemons.forEach(poke => {
       const liNode = document.createElement("li");
       liNode.innerText = poke.name;
       ul.appendChild(liNode);
-    });
-    
+    });    
+  }
+
+  fetch(URL, {
+    method: "GET",
+    "Content-Type": "application/json"
   })
-  .catch(err => {
-    console.error(err.message)
-  })
+  .then(handleResponce)
+  .then(handleData)
+  .catch(err => console.error(err.message))
 
 });
