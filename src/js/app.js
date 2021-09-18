8fdc4e4928a680c82a73db1a6a603c00b54c2820
ptaushanov <pepi15032000@gmail.com>
2021-09-19 00:58:46 +0300

    feat: add fetch get request

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,24 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  const URL = "https://pokeapi.co/api/v2/pokemon?limit=10"
+
+  fetch(URL, {
+    method: "GET",
+    "Content-Type": "application/json"
+  }).then(res => {
+    if(res.status === 200)
+    {
+      return res.json();
+    }
+    else {
+      throw new Error(`Can't get resources. Status: ${res.status}`);
+    }
+  }).then(res => {
+    console.log(res)
+  })
+  .catch(err => {
+    console.error(err.message)
+  })
+
 });
